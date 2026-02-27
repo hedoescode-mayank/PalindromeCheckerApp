9270aee7fc5b3f3ec07ed06257200ce7bfdd8344
hedoescode-mayank <md6074@srmist.edu.in>
2026-02-27 09:59:24 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,22 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized sucessfully");
+
+        // Hardcoded string
+        String text = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed = reversed + text.charAt(i);
+        }
+
+        // Check palindrome
+        boolean isPalindrome = text.equals(reversed);
+
+        // Print output in required format
+        System.out.println("INPUT text : " + text);
+        System.out.println("IS it a Palindrome ?: " + isPalindrome);
+
     }
 }
