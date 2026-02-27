6715c01196dc6d7afc455128eb14d82608d5b41d
hedoescode-mayank <md6074@srmist.edu.in>
2026-02-27 09:33:29 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,7 @@
 public class PalindromeCheckerApp {
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to the palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized sucessfully");
+    }
 }
